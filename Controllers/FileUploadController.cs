56960cdcb83119cbd1b6f0977abc3905388b83a8
Andrew Buteskul <buteskul5.6@gmail.com>
2023-03-16 12:26:34 +0200

    Add project files.

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using AppUploadFile.Models;
+
+namespace AppUploadFile.Controllers
+{
+
+    [Route("api/FileUpload")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly string _connectionString;
+        private readonly string _containerName;
+
+        public FileUploadController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetValue<string>("BlobConnectionString");
+            _containerName = configuration.GetValue<string>("BlobContainerName");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload()
+        {
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                var file = formCollection.Files.First();
+
+                if (file.Length > 0)
+                {
+                    var container = new BlobContainerClient(_connectionString, _containerName);
+                    var createResponse = await container.CreateIfNotExistsAsync();
+
+                    if (createResponse != null && createResponse.GetRawResponse().Status == 201)
+                        await container.SetAccessPolicyAsync(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+
+                    var blob = container.GetBlobClient(file.FileName);
+                    await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
+                    using (var fileStream = file.OpenReadStream())
+                    {
+                        await blob.UploadAsync(fileStream, new BlobHttpHeaders { ContentType = file.ContentType });
+                    }
+
+                    return Ok(blob.Uri.ToString());
+                }
+
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+                
+    }
+
+}
+
---
+++ Models/Form.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppUploadFile.Models
+{
+    public class Form
+    {
+        [Required(ErrorMessage = "Fill in the email")]
+        public string Email { get; set; }
+
+        //[Required]
+        //[FileExtensions(Extensions = "docx", ErrorMessage ="The file must be only type of 'docx'")]
+        //public string FileName { get; set; }
+    }
+}
